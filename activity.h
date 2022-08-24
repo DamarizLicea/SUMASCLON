6d83318d641c6837796da8fdc167eade5b3364c3
Damariz Licea <A01369045@itesm.mx>
2022-08-24 15:22:16 -0500

    Actividad finalizada

--- activity.h
+++ activity.h
@@ -1,12 +1,14 @@
 // =================================================================
 //
 // File: activity.h
-// Author:
-// Date:
+// Author: Damariz Licea Carrisoza
+// Date: 24/08/22
 //
 // =================================================================
 #ifndef ACTIVITY_H
 #define ACTIVITY_H
+#include <iostream>
+using namespace std;
 
 // =================================================================
 // sumaIterativa. Calculate the sum from 1 to n with an iterative
@@ -14,10 +16,16 @@
 //
 // @param n	The maximum limit of the sum.
 // @return	The result of the addition from 1 to n.
-// @Complexity	??
+// @Complexity	O(n)
 // =================================================================
+
 unsigned int sumaIterativa(unsigned int n) {
-	return 0;
+	int acum=0;
+	for(int i=1;i<=n;i++){
+		acum=acum+i;
+
+	}
+	return acum;
 }
 
 // =================================================================
@@ -26,11 +34,22 @@ unsigned int sumaIterativa(unsigned int n) {
 //
 // @param n	The maximum limit of the sum.
 // @return	The result of the addition from 1 to n.
-// @Complexity	??
+// @Complexity	O(n)
 // =================================================================
+
+
 unsigned int sumaRecursiva(unsigned int n) {
-	return 0;
+
+	if (n==1){
+		return 1;
+	}
+	if (n<=0){
+		return 0;
+	}
+	return n + sumaRecursiva(n-1);
+
 }
+
 
 // =================================================================
 // sumaDirecta. Calculate the sum from 1 to n with a mathematical
@@ -38,10 +57,11 @@ unsigned int sumaRecursiva(unsigned int n) {
 //
 // @param n	The maximum limit of the sum.
 // @return	The result of the addition from 1 to n.
-// @Complexity	??
+// @Complexity O(1)	
 // =================================================================
 unsigned int sumaDirecta(unsigned int n) {
-	return 0;
+
+	return (n*(n+1))/2;
 }
 
 #endif /* ACTIVITY_H */
